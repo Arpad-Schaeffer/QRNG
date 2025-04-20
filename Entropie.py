f7a89600414a028acdf5761ea0194388420f5e9c
Arpad Schaeffer <schaeffer.apd@gmail.com>
2025-04-20 17:51:31 +0200

    trj

--- Entropie.py
+++ Entropie.py
@@ -9,26 +9,28 @@ difference = [a - b for a, b in zip(shot_noise, elec_noise)]
 ratio = [a / b for a, b in zip(elec_noise, shot_noise)]
 
 # Indices pour l'abscisse
-x = range(len(shot_noise))
-plt.plot(x, shot_noise, label='Shot Noise', color='blue', marker='o')  # Ronds pour Shot Noisea
-plt.plot(x, elec_noise, label='Elec Noise', color='red', marker='s')  # Carrés pour Elec Noise
-plt.legend()
+x = range(1,len(shot_noise)+1)
+plt.style.use('bmh')  # Utilisation du style 'bmh' pour le graphique
+plt.plot(x, shot_noise, label=r"$H(X)_{tot}$", color='blue', marker='o')  # Ronds pour Shot Noisea
+plt.plot(x, elec_noise, label=r"$H(X)_{elec}$", color='red', marker='s')  # Carrés pour Elec Noise
+plt.legend(fontsize = 12)
 plt.xlabel('Nombre de bits associés')
-plt.ylabel('Entropie')
-plt.title('Comparaison entre Shot Noise et Elec Noise')
+plt.ylabel('Entropie (bits)')
+plt.title(r"Entropies totale $H(X)_{tot}$ et électrique $H(X)_{elec}$ en fonction du nombre de bits associés")
 plt.show()
 # Création de la figure et des sous-graphiques
 fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, gridspec_kw={'height_ratios': [3, 1]})
 
 # Tracé des deux listes sur le premier graphique avec des symboles
-ax1.scatter(x, difference, label='Entropie quantique', color='blue', marker='o')  # Ronds pour Shot Noise
-ax1.set_ylabel('Valeurs')
+ax1.scatter(x, difference, label=r"Entropie quantique $H(X)_{tot}-H(X)_{elec}$", color='blue', marker='o')  # Ronds pour Shot Noise
+ax1.set_ylabel('Entropie quantique (bits)')
 ax1.legend()
 
 # Tracé de la différence sur le deuxième graphique avec des carrés reliés par des traits
-ax2.plot(x, ratio, label='Ratio', color='green', marker='s', linestyle='-')  # Carrés pour la différence
-ax2.set_xlabel('Index')
-ax2.set_ylabel('Rapport entre Shot Noise et Elec Noise')
+ax2.plot(x, ratio, color='green', marker='s', linestyle='')  # Carrés pour la différence
+ax2.set_xlabel('Nombre de bits associés')
+ax2.set_ylabel(r"$\frac{H(X)_{elec}}{H(X)_{tot}}$",fontsize = 15)
+ax2.set_ylim(0.7, 1.1)  # Limite de l'axe y pour le deuxième graphique
 ax2.legend()
 
 # Ajustement de l'espacement entre les sous-graphiques
